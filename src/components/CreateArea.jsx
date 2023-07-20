076924366e31119ba3573e214ece73d279b29fa6
Aditya <adityakrjha111@gmai.com>
2023-07-21 00:09:13 +0530

    Updated website with materail-ui

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
+import { useEffect, useRef } from "react"; // to set ref to the div 
 
 function CreateArea(props) {
 
   // const [title, setTitle] = useState("");
   // const [text, setText] = useState("");
   // using the below method instead of creating multiple useStates like above.
+  const [isExpanded, setExpanded] = useState(false);
 
   const [noteText, setNoteText] = useState({
     title: "",
@@ -28,25 +33,51 @@ function CreateArea(props) {
     event.preventDefault();
   }
 
+  function expand() {
+    setExpanded(true);
+  }
+
+  // This code below is for setting 
+  // isExpanded to false if outside click is detected :
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside)
+  }, [])
+
+  const refOne = useRef(null);
+
+  const handleClickOutside = (e) => {
+    if(!refOne.current.contains(e.target)){
+      setExpanded(false);
+    }
+  }
+
   return (
     <div>
-      <form className="create-note">
-        <input
-          onChange={handleTitleChange}
-          name="title"
-          value={noteText.title}
-          placeholder="Title"
+      <form className="create-note" ref={refOne}>
+        {isExpanded && (
+          <input
+            onChange={handleTitleChange}
+            name="title"
+            value={noteText.title}
+            placeholder="Title"
 
-        />
+          />
+        )}
+
 
         <textarea
+          onClick={expand}
           onChange={handleTitleChange}
           name="content"
           value={noteText.content}
           placeholder="Take a note..."
-          rows="3" />
+          rows={isExpanded ? 3 : 1} />
 
-        <button onClick={submit}>Add</button>
+        <Zoom in={isExpanded}>
+          <Fab onClick={submit}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
 
       </form>
     </div>
